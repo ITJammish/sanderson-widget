787a5bae84af5b2efc0b21cfb854fc7690d0f77e
itjscott <jimboscott32@gmail.com>
2022-05-18 13:14:57 +0100

    Added common styles for use in differen layout files. Added helper methods for styling widget background per API version.

--- app/src/main/java/com/itj/sandersonwidget/ui/layouts/LayoutProvider.kt
+++ app/src/main/java/com/itj/sandersonwidget/ui/layouts/LayoutProvider.kt
@@ -57,28 +57,33 @@ class LayoutProvider {
 
         return when (gridSize.width) {
             is Small -> when (gridSize.height) {
-                is Small -> fetchSmallSmallView(context, progressItemData)
-                is Medium -> fetchSmallMediumView(context, progressItemData, appWidgetId)
-                is Large -> fetchSmallLargeView(context, progressItemData, appWidgetId)
+                is Small -> fetchSmallSmallView(context, progressItemData, width, height)
+                is Medium -> fetchSmallMediumView(context, progressItemData, appWidgetId, width, height)
+                is Large -> fetchSmallLargeView(context, progressItemData, appWidgetId, width, height)
             }
             is Medium -> when (gridSize.height) {
-                is Small -> fetchMediumSmallView(context, progressItemData, appWidgetId)
-                is Medium -> fetchMediumMediumView(context, progressItemData, appWidgetId)
-                is Large -> fetchMediumLargeView(context, progressItemData, appWidgetId)
+                is Small -> fetchMediumSmallView(context, progressItemData, appWidgetId, width, height)
+                is Medium -> fetchMediumMediumView(context, progressItemData, appWidgetId, width, height)
+                is Large -> fetchMediumLargeView(context, progressItemData, appWidgetId, width, height)
             }
             is Large -> when (gridSize.height) {
                 is Small -> fetchLargeSmallView(context, progressItemData, appWidgetId, width, height)
-                is Medium -> fetchLargeMediumView(context, progressItemData, appWidgetId)
-                is Large -> fetchLargeLargeView(context, progressItemData, appWidgetId)
+                is Medium -> fetchLargeMediumView(context, progressItemData, appWidgetId, width, height)
+                is Large -> fetchLargeLargeView(context, progressItemData, appWidgetId, width, height)
             }
         }
-//        return oldViews(context, appWidgetId)
     }
 
     // Todo scrollable list, not just first item
-    private fun fetchSmallSmallView(context: Context, progressItemData: List<ProgressItem>): RemoteViews {
+    private fun fetchSmallSmallView(
+        context: Context,
+        progressItemData: List<ProgressItem>,
+        width: Int,
+        height: Int,
+    ): RemoteViews {
         return RemoteViews(context.packageName, R.layout.view_small_small).apply {
             bindProgressItem(context, progressItemData[0], R.id.item_progress_indicator, R.id.item_title)
+            bindStyledBackgroundCompat(context, width, height)
         }
     }
 
@@ -86,10 +91,13 @@ class LayoutProvider {
         context: Context,
         progressItemData: List<ProgressItem>,
         appWidgetId: Int,
+        width: Int,
+        height: Int,
     ): RemoteViews {
         return RemoteViews(context.packageName, R.layout.view_small_medium).apply {
             bindProgressItem(context, progressItemData[0], R.id.item_progress_indicator_1, R.id.item_title_1)
             bindProgressItemList(context, appWidgetId, 3)
+            bindStyledBackgroundCompat(context, width, height)
         }
     }
 
@@ -97,11 +105,14 @@ class LayoutProvider {
         context: Context,
         progressItemData: List<ProgressItem>,
         appWidgetId: Int,
+        width: Int,
+        height: Int,
     ): RemoteViews {
         return RemoteViews(context.packageName, R.layout.view_small_large).apply {
             bindProgressItem(context, progressItemData[0], R.id.item_progress_indicator_1, R.id.item_title_1)
             bindProgressItem(context, progressItemData[1], R.id.item_progress_indicator_2, R.id.item_title_2)
             bindProgressItemList(context, appWidgetId, 2)
+            bindStyledBackgroundCompat(context, width, height)
         }
     }
 
@@ -109,10 +120,13 @@ class LayoutProvider {
         context: Context,
         progressItemData: List<ProgressItem>,
         appWidgetId: Int,
+        width: Int,
+        height: Int,
     ): RemoteViews {
         return RemoteViews(context.packageName, R.layout.view_medium_small).apply {
             bindProgressItem(context, progressItemData[0], R.id.item_progress_indicator_1, R.id.item_title_1)
             bindProgressItemList(context, appWidgetId, 3)
+            bindStyledBackgroundCompat(context, width, height)
         }
     }
 
@@ -120,6 +134,8 @@ class LayoutProvider {
         context: Context,
         progressItemData: List<ProgressItem>,
         appWidgetId: Int,
+        width: Int,
+        height: Int,
     ): RemoteViews {
         return if (articlesEnabled) {
             RemoteViews(context.packageName, R.layout.view_medium_medium_with_articles).apply {
@@ -128,7 +144,7 @@ class LayoutProvider {
                 bindArticleStack(context, appWidgetId)
             }
         } else {
-            fetchFourByFourView(context, progressItemData)
+            fetchFourByFourView(context, progressItemData, width, height)
         }
     }
 
@@ -136,6 +152,8 @@ class LayoutProvider {
         context: Context,
         progressItemData: List<ProgressItem>,
         appWidgetId: Int,
+        width: Int,
+        height: Int,
     ): RemoteViews {
         return if (articlesEnabled) {
             RemoteViews(context.packageName, R.layout.view_medium_medium_with_articles).apply {
@@ -144,7 +162,7 @@ class LayoutProvider {
                 bindArticleStack(context, appWidgetId)
             }
         } else {
-            fetchFourByFourView(context, progressItemData)
+            fetchFourByFourView(context, progressItemData, width, height)
         }
     }
 
@@ -159,56 +177,7 @@ class LayoutProvider {
             bindProgressItem(context, progressItemData[0], R.id.item_progress_indicator_1, R.id.item_title_1)
             bindProgressItem(context, progressItemData[1], R.id.item_progress_indicator_2, R.id.item_title_2)
             bindProgressItemList(context, appWidgetId, 2)
-
-            // todo after lunch:
-            // Either set up themes and save their int to prefs on settings option change
-
-            // also need to clean this up vv, for API level AND test on other widget sizes...
-
-            //Not needed for later API -> todo @API:setStyledBackgroundImage()
-            // or: getBackgroundDrawableId -> if(api>=31){}/else{}
-            // Calculate widget dimensions
-            val screenDensity = context.resources.displayMetrics.density
-            val widgetWidth = (screenDensity * width).toInt()
-            val widgetHeight = (screenDensity * height).toInt()
-
-            // Decode styled scaled background image
-            val backgroundAttrs = intArrayOf(R.attr.appWidgetBackgroundImage)
-            // todo pull style id out of shared prefs/create a keymap for enum -> style that can be queried
-            val styledBackgroundAttrs = context.obtainStyledAttributes(
-                themeResId,
-                backgroundAttrs
-            )
-            val backgroundResId = styledBackgroundAttrs.getResourceId(0, R.drawable.oathbringer)
-            styledBackgroundAttrs.recycle()
-
-            val options = BitmapFactory.Options().apply {
-                inMutable = true
-            }
-            val result = Bitmap.createBitmap(widgetWidth, widgetHeight, Bitmap.Config.ARGB_8888)
-            // todo get drawable id from user set style/theme attr
-            val rawBitmap = BitmapFactory.decodeResource(context.resources, backgroundResId, options)
-            val bitmap = Bitmap.createScaledBitmap(rawBitmap, widgetWidth, widgetHeight, true)
-            val shader = BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
-            val paint = Paint().apply {
-                isAntiAlias = true
-                this.shader = shader
-            }
-
-            // Obtain corner radius from styles
-            val attrs = intArrayOf(R.attr.appWidgetRadius)
-            val styledAttr = context.obtainStyledAttributes(R.style.Theme_SandersonWidget_AppWidgetContainer, attrs)
-            val radius = styledAttr.getDimension(0, 0f)
-            styledAttr.recycle()
-
-            // Cut background image corners and apply to image view
-            val rect = RectF(0.0f, 0.0f, widgetWidth.toFloat(), widgetHeight.toFloat())
-            Canvas(result).apply { drawRoundRect(rect, radius, radius, paint) }
-            setImageViewBitmap(R.id.widget_background_view, result)
-
-            // usable for higher APIs (31 etc)
-            // todo get drawable id from user set style/theme attr
-//            setImageViewResource(R.id.widget_backgroun_view, R.drawable.wayofkings)
+            bindStyledBackgroundCompat(context, width, height)
         }
     }
 
@@ -216,6 +185,8 @@ class LayoutProvider {
         context: Context,
         progressItemData: List<ProgressItem>,
         appWidgetId: Int,
+        width: Int,
+        height: Int,
     ): RemoteViews {
         return if (articlesEnabled) {
             RemoteViews(context.packageName, R.layout.view_large_medium_with_articles).apply {
@@ -225,7 +196,7 @@ class LayoutProvider {
                 bindArticleStack(context, appWidgetId)
             }
         } else {
-            fetchFourByFourView(context, progressItemData)
+            fetchFourByFourView(context, progressItemData, width, height)
         }
     }
 
@@ -233,6 +204,8 @@ class LayoutProvider {
         context: Context,
         progressItemData: List<ProgressItem>,
         appWidgetId: Int,
+        width: Int,
+        height: Int,
     ): RemoteViews {
         return if (articlesEnabled) {
             RemoteViews(context.packageName, R.layout.view_large_medium_with_articles).apply {
@@ -242,17 +215,23 @@ class LayoutProvider {
                 bindArticleStack(context, appWidgetId)
             }
         } else {
-            fetchFourByFourView(context, progressItemData)
+            fetchFourByFourView(context, progressItemData, width, height)
         }
     }
 
     // -- Fetch common layout methods --
-    private fun fetchFourByFourView(context: Context, progressItemData: List<ProgressItem>): RemoteViews {
+    private fun fetchFourByFourView(
+        context: Context,
+        progressItemData: List<ProgressItem>,
+        width: Int,
+        height: Int,
+    ): RemoteViews {
         return RemoteViews(context.packageName, R.layout.view_medium_medium).apply {
             bindProgressItem(context, progressItemData[0], R.id.item_progress_indicator_1, R.id.item_title_1)
             bindProgressItem(context, progressItemData[1], R.id.item_progress_indicator_2, R.id.item_title_2)
             bindProgressItem(context, progressItemData[2], R.id.item_progress_indicator_3, R.id.item_title_3)
             bindProgressItem(context, progressItemData[3], R.id.item_progress_indicator_4, R.id.item_title_4)
+            bindStyledBackgroundCompat(context, width, height)
         }
     }
 
@@ -317,7 +296,6 @@ class LayoutProvider {
         val articleClickIntent = Intent(context, ProgressBarsWidgetProvider::class.java).apply {
             action = ProgressBarsWidgetProvider.ACTION_ARTICLE_CLICK
         }
-        // Todo extract method to handle SDK checks more cleanly
         val clickArticlePendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             PendingIntent.getBroadcast(context, 0, articleClickIntent, PendingIntent.FLAG_MUTABLE)
         } else {
@@ -328,5 +306,75 @@ class LayoutProvider {
         setRemoteAdapter(R.id.article_stack, articleStackServiceIntent)
         setEmptyView(R.id.article_stack, R.id.article_stack_empty_view)
         setPendingIntentTemplate(R.id.article_stack, clickArticlePendingIntent)
+    }
+
+    private fun RemoteViews.bindStyledBackgroundCompat(
+        context: Context,
+        width: Int,
+        height: Int,
+    ) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            bindStyledBackground(context)
+        } else {
+            bindStyledBackgroundManual(context, width, height)
+        }
+    }
+
+    private fun RemoteViews.bindStyledBackground(context: Context) {
+        // Decode styled scaled background image
+        val backgroundAttrs = intArrayOf(R.attr.appWidgetBackgroundImage)
+        // todo pull style id out of shared prefs/create a keymap for enum -> style that can be queried
+        val styledBackgroundAttrs = context.obtainStyledAttributes(
+            themeResId,
+            backgroundAttrs
+        )
+        val backgroundResId = styledBackgroundAttrs.getResourceId(0, R.drawable.way_of_kings)
+        styledBackgroundAttrs.recycle()
+
+        setImageViewResource(R.id.widget_background_view, backgroundResId)
+    }
+
+    private fun RemoteViews.bindStyledBackgroundManual(
+        context: Context,
+        width: Int,
+        height: Int,
+    ) {
+        // Calculate widget dimensions
+        val screenDensity = context.resources.displayMetrics.density
+        val widgetWidth = (screenDensity * width).toInt()
+        val widgetHeight = (screenDensity * height).toInt()
+
+        // Decode styled scaled background image
+        val backgroundAttrs = intArrayOf(R.attr.appWidgetBackgroundImage)
+        // todo pull style id out of shared prefs/create a keymap for enum -> style that can be queried
+        val styledBackgroundAttrs = context.obtainStyledAttributes(
+            themeResId,
+            backgroundAttrs
+        )
+        val backgroundResId = styledBackgroundAttrs.getResourceId(0, R.drawable.way_of_kings)
+        styledBackgroundAttrs.recycle()
+
+        val options = BitmapFactory.Options().apply {
+            inMutable = true
+        }
+        val result = Bitmap.createBitmap(widgetWidth, widgetHeight, Bitmap.Config.ARGB_8888)
+        val rawBitmap = BitmapFactory.decodeResource(context.resources, backgroundResId, options)
+        val bitmap = Bitmap.createScaledBitmap(rawBitmap, widgetWidth, widgetHeight, true)
+        val shader = BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
+        val paint = Paint().apply {
+            isAntiAlias = true
+            this.shader = shader
+        }
+
+        // Obtain corner radius from styles
+        val attrs = intArrayOf(R.attr.appWidgetRadius)
+        val styledAttr = context.obtainStyledAttributes(R.style.Theme_SandersonWidget_AppWidgetContainer, attrs)
+        val radius = styledAttr.getDimension(0, 0f)
+        styledAttr.recycle()
+
+        // Cut background image corners and apply to image view
+        val rect = RectF(0.0f, 0.0f, widgetWidth.toFloat(), widgetHeight.toFloat())
+        Canvas(result).apply { drawRoundRect(rect, radius, radius, paint) }
+        setImageViewBitmap(R.id.widget_background_view, result)
     }
 }
